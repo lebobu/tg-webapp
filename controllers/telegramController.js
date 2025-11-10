82b47a9a96ac15ab565b13a0457f12061d337280
Bob(lebobu) <157216886+lebobu@users.noreply.github.com>
2025-11-10 15:07:53 +0300

    10-11-25 start dev

--- controllers/telegramController.js
+++ controllers/telegramController.js
@@ -63,7 +63,7 @@ module.exports = (bot) => ({
     reply_markup: {
       inline_keyboard: [
         [{ text:'Каталог 📖', web_app:{ url } }],
-        [{ text:'Помощь ❓', url:'https://polpo-dev.duckdns.org/help.html' }]
+        [{ text:'Помощь ❓', url:url + '/help'}]
       ]
     }
   }
